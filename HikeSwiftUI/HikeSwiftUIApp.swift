77cc18175d0a575c0f9ad5abd3377369fbe5648b
Dip Dutt <emdutt456@gmail.com>
2025-05-20 18:10:35 +0600

    Initial Commit

---
+++ HikeSwiftUI/HikeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeSwiftUIApp.swift
+//  HikeSwiftUI
+//
+//  Created by Dip on 20/5/25.
+//
+
+import SwiftUI
+
+@main
+struct HikeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
