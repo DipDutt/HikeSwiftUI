b86d60bc5c576d0d365ab030e0c37d5b60d61ee1
Dip Dutt <emdutt456@gmail.com>
2025-07-01 17:34:30 +0600

    add image, color set ,app icons

---
+++ HikeSwiftUI/CardView.swift
@@ -0,0 +1,24 @@
+//
+//  CardView.swift
+//  HikeSwiftUI
+//
+//  Created by Dip on 1/7/25.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        ZStack {
+            Circle()
+                .fill(
+                    LinearGradient(colors: [Color(""), Color("")], startPoint: <#T##UnitPoint#>, endPoint: <#T##UnitPoint#>)
+                    
+                )
+        }
+    }
+}
+
+#Preview {
+    CardView()
+}
