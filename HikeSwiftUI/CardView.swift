2941289104c97801a8c0bc2003c2cbb1a758e014
Dip Dutt <emdutt456@gmail.com>
2025-07-07 21:44:09 +0600

    [Feature] add frame in the CardView

--- HikeSwiftUI/CardView.swift
+++ HikeSwiftUI/CardView.swift
@@ -13,6 +13,7 @@ struct CardView: View {
             Circle()
                 .fill(
                     LinearGradient(colors: [Color("ColorIndigoMedium"), Color("ColorIndigoMedium 1")], startPoint:.topLeading, endPoint: .bottomTrailing))
+                .frame(width: 400, height: 300)
             VStack {
                 Image(.image1)
                     .resizable()
