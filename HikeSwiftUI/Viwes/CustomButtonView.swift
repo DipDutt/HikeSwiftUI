ba253bcea4da593cb4591fa3bc59cdc2f39043e3
Dip Dutt <emdutt456@gmail.com>
2025-07-09 18:26:41 +0600

    [Feature] create custom button addind zstack along with two circle,stroke modifier and system image.

---
+++ HikeSwiftUI/Viwes/CustomButtonView.swift
@@ -0,0 +1,53 @@
+//
+//  CustomButtonView.swift
+//  HikeSwiftUI
+//
+//  Created by Dip on 9/7/25.
+//
+
+import SwiftUI
+
+struct CustomButtonView: View {
+    var body: some View {
+        ZStack {
+            Circle()
+                .fill(
+                  LinearGradient(
+                    colors: [
+                      .white,
+                        .colorGreenLight,
+                        .colorGreenMedium],
+                    startPoint: .top,
+                    endPoint: .bottom)
+                )
+            
+            Circle()
+              .stroke(
+                LinearGradient(
+                  colors: [
+                    .colorGrayLight,
+                    .colorGrayMedium],
+                  startPoint: .top,
+                  endPoint: .bottom),
+                lineWidth: 4)
+            
+            Image(systemName: "figure.hiking")
+              .fontWeight(.black)
+              .font(.system(size: 30))
+              .foregroundStyle(
+                LinearGradient(
+                  colors: [
+                    .colorGrayLight,
+                    .colorGrayMedium],
+                  startPoint: .top,
+                  endPoint: .bottom)
+              )
+        }// end of zstck
+        .frame(width: 100, height: 100)
+    }
+}
+
+#Preview {
+    CustomButtonView()
+        .padding()
+}
