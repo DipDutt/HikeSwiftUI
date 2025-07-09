0d3e2c92b8157dd7fc2b549d6a2e7c5c5476a3fe
Dip Dutt <emdutt456@gmail.com>
2025-07-09 17:42:35 +0600

    add card view in the MainView

---
+++ HikeSwiftUI/Viwes/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  HikeSwiftUI
+//
+//  Created by Dip on 9/7/25.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        CardView()
+    }
+}
+
+#Preview {
+    MainView()
+}
