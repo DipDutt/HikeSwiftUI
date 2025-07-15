1b1097302019cf5945d2ef16f840e93a2339a448
Dip Dutt <emdutt456@gmail.com>
2025-07-16 01:11:36 +0600

    [Feature] add animation and linear gradient to the Circle

---
+++ HikeSwiftUI/Views/CustomCircleView.swift
@@ -0,0 +1,39 @@
+//
+//  CustomCircleView.swift
+//  HikeSwiftUI
+//
+//  Created by Dip on 16/7/25.
+//
+
+import SwiftUI
+
+struct CustomCircleView: View {
+    @State private var isAnimateGradient: Bool = false
+    var body: some View {
+        ZStack {
+            Circle()
+                .fill(
+                    LinearGradient(
+                        colors: [
+                            .colorIndigoMedium,
+                            .colorIndigoMedium1
+                        ],
+                        startPoint: isAnimateGradient ? .topLeading : .bottomLeading,
+                        endPoint: isAnimateGradient ? .bottomTrailing : .topTrailing
+                    )
+                )
+                .onAppear {
+                    withAnimation(Animation.linear(duration: 3).repeatForever(autoreverses: true)) {
+                        self.isAnimateGradient.toggle()
+                    }
+
+                }
+                
+        }
+        .frame(width: 300, height: 300)
+    }
+}
+
+#Preview {
+    CustomCircleView()
+}
