ceaebd5bb0653329c40978a371fc6fe0178f9cf4
Dip Dutt <emdutt456@gmail.com>
2025-07-16 15:01:35 +0600

    add MainView in a Folder called screen and Create another view called SettingsView for present as a sheet.

---
+++ HikeSwiftUI/Screen/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  HikeSwiftUI
+//
+//  Created by Dip on 16/7/25.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
